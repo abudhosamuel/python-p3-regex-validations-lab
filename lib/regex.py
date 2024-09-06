79fb7d0910c8361fe06ca0659b4142255382182f
abudhosamuel <abudhosamuel@gmail.com>
2024-09-06 20:43:53 +0300

    regex functions

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Regular expression for name, accounting for names with hyphens and apostrophes
+name = r"^[A-Z][a-z]+(?:'[A-Z][a-z]+)?(?: [A-Z][a-z]+)*(?:-[A-Z][a-z]+)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Regular expression for phone number, accounting for numbers with or without delimiters
+phone_number = r"^(\(\d{3}\) |\d{3}-|\d{10})\d{3}-?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Regular expression for email address, allowing numbers at the start
+email_address = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
